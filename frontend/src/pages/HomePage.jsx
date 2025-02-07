112acef1c1a0c9c6bc84d73db473b95e2357c087
Parthav <parthavsabrwal@gmail.com>
2025-02-07 11:10:50 +0530

    Carousel Added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer';
+import Footer from '../components/Footer/Footer';
+import Carousel from '../components/Carousel/Carousel';
 
 function HomePage() {
   return (
     <>
     <Navbar />
+    <Carousel />
     <Footer />
     </>
   );
